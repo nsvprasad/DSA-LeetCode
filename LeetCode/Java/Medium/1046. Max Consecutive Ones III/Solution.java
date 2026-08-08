fbb40b166b0f284e6f1ab4566bf7451ab822023c
Naga Siva VaraPrasad.P <nsvprasadperavali@gmail.com>
2026-08-08 14:55:58 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/1046. Max Consecutive Ones III/Solution.java
+++ LeetCode/Java/Medium/1046. Max Consecutive Ones III/Solution.java
@@ -1,21 +1,5 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-           int n=nums.length;
-           int zerocount=0;
-           int left=0;
-
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                zerocount++;
-
-            }if(zerocount>k){
-                if(nums[left]==0){
-                    zerocount--;
-                }
-                left++;
-            }
-        }
-        return n-left;
-
-        }
+        
     }
+}
