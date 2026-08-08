f38172de8b2a7c22382dd440028f4353e28e9ebd
Naga Siva VaraPrasad.P <nsvprasadperavali@gmail.com>
2026-08-08 14:53:23 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/9. Palindrome Number/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       if(x<0){
+        return false;
+       }
+       String s=String.valueOf(x);
+       String rev=new StringBuilder(s).reverse().toString();
+       
+       return  s.equals(rev);
+    }
+}
